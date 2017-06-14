918b5598d9c25d789a83705d9167ce4f14200af0
CarVac <c.lo.to.da.down.lo@gmail.com>
2017-06-13 22:52:31 -0400

    Fix my relative path

--- filmulator-gui/main.cpp
+++ filmulator-gui/main.cpp
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
         engine.load("/usr/lib/filmulator-gui/qml/filmulator-gui/main.qml");
     }
     */
-    else if (QFile("/usr/qml/main.qml").exists())
+    else if (QFile("../../usr/qml/main.qml").exists())
     {
         engine.load("../../usr/qml/main.qml");
     }
